98c91493eadd6c543195edb5f6dac422226709e1
Steven Xie <dev@stevenxie.me>
2019-10-11 15:05:35 -0400

    zero: Add 'Entry' to Logrus no-ops

--- zero/logrus.go
+++ zero/logrus.go
@@ -9,11 +9,19 @@ import (
 // Logger returns a no-op logrus.Logger.
 func Logger() *logrus.Logger { return noopLogger }
 
-var noopLogger *logrus.Logger
+// Entry returns a no-op logrus.Entry.
+func Entry() *logrus.Entry { return noopEntry }
 
-// Initialize noopLogger.
+var (
+	noopLogger *logrus.Logger
+	noopEntry  *logrus.Entry
+)
+
+// Initialize noopLogger, noopEntry.
 func init() {
 	noopLogger = logrus.New()
 	noopLogger.SetLevel(logrus.PanicLevel)
 	noopLogger.SetOutput(ioutil.Discard)
+
+	noopEntry = logrus.NewEntry(noopLogger)
 }
