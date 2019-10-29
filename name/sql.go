77723e3c372255590cc198f6ec5d85d4d920faac
Steven Xie <dev@stevenxie.me>
2019-10-29 14:42:32 -0400

    Deprecatre 'SQLTableFor' in favor of 'SQLTable'
    Table names created with 'SQLTableFor' are just... really ugly and
    sometimes unintuitive.

--- name/sql.go
+++ name/sql.go
@@ -1,30 +1,11 @@
 package name
 
-import (
-	"reflect"
-	"strings"
+import "strings"
 
-	"github.com/cockroachdb/errors"
-	"github.com/iancoleman/strcase"
-	"go.stevenxie.me/gopkg/zero"
-)
-
-// SQLTableFor derives an SQL table name from a value's type name.
-//
-// It returns a snake-cased string containing the value's full type name
-// (excluding the package domain).
-func SQLTableFor(v zero.Interface) string {
-	t := getElem(v)
-	if t.Kind() == reflect.Func {
-		panic(errors.New("name: kind may not be 'reflect.Func'"))
+// SQLTable snake-cases the given parts to derive an SQL table name.
+func SQLTable(parts ...string) string {
+	for i, part := range parts {
+		parts[i] = strings.ToLower(part)
 	}
-	path := t.PkgPath()
-	if i := strings.LastIndexByte(path, '.'); i >= 0 {
-		path = path[i+1:]
-		if j := strings.IndexByte(path, '/'); j >= 0 {
-			path = path[j+1:]
-		}
-	}
-	path = strings.ReplaceAll(path, "/", "_")
-	return path + "_" + strcase.ToSnake(t.Name())
+	return strings.Join(parts, "_")
 }
